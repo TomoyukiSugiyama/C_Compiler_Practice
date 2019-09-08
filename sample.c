996f4c1a25fa10e631bef6a37b5de045f367ce1f
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2019-09-09 00:06:26 +0900

    feat:トークナイズする関数を一部実装

--- sample.c
+++ sample.c
@@ -1,6 +1,95 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+//　トークンの種類
+typedef enum {
+    TK_RESERVED,    //　文字
+    TK_NUM,         //　整数トークン
+    TK_EOF,         //　入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+//　トークン型
+struct Token {
+    TokenKind kind; //　トークンの型
+    Token *next;    //　次の入力トークン
+    int val;        //　kindがTK_NUMの場合、その数値
+    char *str;      //　トークン文字列
+};
+
+//　現在着目しているトークン
+Token *token;
+
+//　エラーを報告するための関数
+//　printfと同じ引数を取る
+void error(char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+//　次のトークンが期待している記号のときには、トークンを１つ読み進めて
+//　真を返す。それ以外の場合には偽を返す。
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        return false;
+    token = token->next;
+    return true;
+}
+
+//　次のトークンが期待している記号のときには、トークンを１つ読み進める。
+//　それ以外の場合にはエラーを報告する。
+void expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        error("'%c'ではありません", op);
+    token = token->next;
+}
+
+//　次のトークンが数値の場合、トークンを１つ読み進めてその数値を返す。
+//　それ以外の場合にはエラーを報告する。
+int expect_number() {
+    if (token->kind != TK_NUM)
+        error("数ではありません");
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+//　新しいトークンを作成してcurを繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+//　入力文字列pをトークナイズしてそれを返す
+Token *tokenaize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        //　空白文字をスキップ
+        if (isspece(*p)){
+            p++;
+            continue;
+        }
+        
+    }
+}
 int main(int argc, char **argv){
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
