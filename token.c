cf18de7a77a8f8b6e700042cccd0c7e580ea752a
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-04-19 09:56:55 +0900

    トークナイズ関連のコードをファイル分割

---
+++ token.c
@@ -0,0 +1,71 @@
+#include "9cc.h"
+
+//　新しいトークンを作成してcurを繋げる
+static Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    tok->len = len;
+    cur->next = tok;
+    return tok;
+}
+
+//　入力文字列pをトークナイズしてそれを返す
+Token *tokenize() {
+    char *p = user_input;
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        //　空白文字をスキップ
+        if (isspace(*p)){
+            p++;
+            continue;
+        }
+
+        if (!strncmp(p, "==", 2) ||
+            !strncmp(p, "!=", 2)){
+            cur = new_token(TK_RESERVED, cur, p, 2);
+            p+=2;
+            continue;
+        } 
+
+        if (!strncmp(p, "<=", 2) ||
+            !strncmp(p, ">=", 2)){ 
+            cur = new_token(TK_RESERVED, cur, p, 2);
+            p+=2;
+            continue;
+        }
+
+        if (!strncmp(p, "<", 1) ||
+            !strncmp(p, ">", 1)){
+            cur = new_token(TK_RESERVED, cur, p++, 1);
+            continue;
+        } 
+
+        if (*p == '+' || *p == '-' || 
+            *p == '*' || *p == '/' ||
+            *p == '(' || *p == ')' ||
+            *p == ';' || *p == '='){
+            cur = new_token(TK_RESERVED, cur, p++, 1);
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p, 0);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        if('a' <= *p && *p <= 'z') {
+            cur = new_token(TK_IDENT, cur, p++, 0);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+        error_at(p,"トークナイズできません");
+    }
+
+    new_token(TK_EOF, cur, p, 0);
+    return head.next;
+}
